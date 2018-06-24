39578a9856ca5d32f823d75222fba0cdf8bd1ccc
ssdorwat <ssdorwat@ncsu.edu>
2018-06-24 19:07:55 -0400

    added editing

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,47 @@
+class ArticlesController < ApplicationController
+
+    def new
+        @article = Article.new
+    end
+
+    def index
+        @articles = Article.all
+    end
+
+    def create
+        # render plain: params[:article].inspect
+        @article= Article.new(article_params)
+        if @article.save
+            flash[:notice] = "article is successfully stored"
+            redirect_to article_path(@article)
+        else
+            render 'new'
+        end
+    end
+
+    def show
+        @article = Article.find(params[:id])
+    end
+
+    def edit
+        @article = Article.find(params[:id])
+    end
+
+    def update 
+        @article = Article.find(params[:id])
+         if @article.update(article_params)
+            flash[:notice] = "article is successfully updated"
+            redirect_to article_path(@article)
+        else
+            render 'edit'
+        end
+    end
+
+    private
+    def article_params
+        params.require(:article).permit(:title, :description)
+    end
+
+end
+
+
