541da52bba35a47f29678aca100496f8effebb17
ssdorwat <ssdorwat@ncsu.edu>
2018-06-25 02:03:40 -0400

    signup

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,43 @@
+class UsersController < ApplicationController
+    before_action :set_user , only: [ :show]
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            flash[:success] = "Welcome to Alpha blog #{@user.username}"
+            redirect_to articles_path
+        else 
+            render 'new'
+        end
+    end
+
+    def show
+
+    end
+
+    def destroy
+
+    end
+
+    def edit
+
+    end
+
+    def update
+
+    end
+
+    private
+
+    def set_user
+        @user = User.find(params[:id])    
+    end    
+
+    def user_params
+        params.require(:user).permit(:username, :email, :password)
+    end
+
+end
