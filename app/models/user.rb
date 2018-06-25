bdf42a67050a793381a3c53e11974383989ba04e
ssdorwat <ssdorwat@ncsu.edu>
2018-06-25 00:31:54 -0400

    validation working

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ApplicationRecord
 
+    validates :username, presence: true, length: {minimum: 3, maximum: 25}, 
+                uniqueness: {case_sensetive: false}
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i        
+    validates :email, presence: true, length: {maximum: 50}, uniqueness: {case_sensetive: false},
+                format: { with: VALID_EMAIL_REGEX }
+    
+    
 end
