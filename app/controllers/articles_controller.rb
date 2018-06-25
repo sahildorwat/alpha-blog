b59a2db74bbdf836853d6d4e8c25d761967e9901
ssdorwat <ssdorwat@ncsu.edu>
2018-06-25 14:26:07 -0400

    small changes to current user

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,7 +15,7 @@ class ArticlesController < ApplicationController
     def create
         # render plain: params[:article].inspect
         @article= Article.new(article_params)
-        @article.user = User.first;
+        @article.user = current_user;
         if @article.save
             flash[:success] = "article is successfully stored"
             redirect_to article_path(@article)
