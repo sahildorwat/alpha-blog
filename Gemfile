ab82ceca5f5756cfc90c53c9b5d81f29181abac1
ssdorwat <ssdorwat@ncsu.edu>
2018-06-25 04:06:18 -0400

    added pagination

--- Gemfile
+++ Gemfile
@@ -37,6 +37,8 @@ gem 'jbuilder', '~> 2.5'
 gem 'bootsnap', '>= 1.1.0', require: false
 gem 'jquery-rails'
 gem 'bcrypt', '~> 3.1.7'
+gem 'will_paginate' , '3.1.5'
+gem 'bootstrap-will_paginate', '1.0.0'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
     before_action :set_user , only: [ :show, :edit, :update]
+
     def new
         @user = User.new
     end
@@ -15,11 +16,12 @@ class UsersController < ApplicationController
     end
 
     def index 
-        @users = User.all
+        @users = User.paginate(page: params[:page], per_page: 5)
+        
     end
 
     def show
-        @articles = @user.articles
+        @articles = @user.articles.paginate( page: params[:page], per_page: 5)
     end
 
     def destroy
