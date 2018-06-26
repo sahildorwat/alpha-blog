ec49c1fe2cf4b154bb07fd923196d92c8a953079
ssdorwat <ssdorwat@ncsu.edu>
2018-06-25 23:32:47 -0400

    added working many to many relation

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,7 @@
 class Article < ApplicationRecord
     belongs_to :user
+    has_many :article_categories
+    has_many :categories, through: :article_categories
     validates :user_id, presence: true
     validates :title, presence: true, length: {minimum:3, maximum:10}
     validates :description, presence: true, length: {minimum:3, maximum:10}
