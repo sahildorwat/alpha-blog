67920e4fb54ee4a5f699b5a54b5206c27897c5ed
ssdorwat <ssdorwat@ncsu.edu>
2018-06-26 00:21:12 -0400

    added show page for category

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -55,7 +55,8 @@ class ArticlesController < ApplicationController
         @article = Article.find(params[:id])
     end
     def article_params
-        params.require(:article).permit(:title, :description)
+        # debugger
+        params.require(:article).permit(:title, :description, category_ids: [])
     end
 
     def require_same_user
--- app/models/article.rb
+++ app/models/article.rb
@@ -3,6 +3,6 @@ class Article < ApplicationRecord
     has_many :article_categories
     has_many :categories, through: :article_categories
     validates :user_id, presence: true
-    validates :title, presence: true, length: {minimum:3, maximum:10}
-    validates :description, presence: true, length: {minimum:3, maximum:10}
+    validates :title, presence: true, length: {minimum:3, maximum:50}
+    validates :description, presence: true, length: {minimum:3, maximum:100}
 end
