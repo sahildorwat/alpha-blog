247bd57aebaff8489f65e6837f368dc26ee24715
ssdorwat <ssdorwat@ncsu.edu>
2018-06-26 00:37:31 -0400

    final touchup

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -24,6 +24,21 @@ class CategoriesController < ApplicationController
         @categories = Category.paginate(page: params[:page], per_page: 5)
     end
 
+    def edit
+        @category = Category.find(params[:id])
+    end
+
+    def update
+        @category = Category.find(params[:id])
+        if @category.update(category_params)
+            flash[:success] = "Category successfully updated"
+            redirect_to category_path(@category)
+        else
+            render 'edit'
+        end
+    end
+
+
     private
 
     def category_params
